ef87d2947ea8c4820ca4384912b681c5d199744a
monisha-kv <monisha@keyvalue.systems>
2022-08-27 17:12:40 +0530

    Add Transaction Listing Section

--- src/navigation/rootNavigation.tsx
+++ src/navigation/rootNavigation.tsx
@@ -1,22 +1,30 @@
-import React, { FunctionComponent, useContext, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {FunctionComponent, useContext, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {
   CardStyleInterpolators,
   createStackNavigator,
 } from '@react-navigation/stack';
-import Home from '../screens/Home/Home';
-import Transactions from '../screens/Transactions/Transactions';
 
-const RootNavigation = ({
-}) => {
-    const { Screen, Navigator } = createStackNavigator();
-    return (
-      <NavigationContainer >
-        <Navigator initialRouteName="Home" headerMode="none">
-          <Screen name="Home" options={{ title: 'Home' }} component={Home} />
-          <Screen name="Transactions" options={{ title: 'Transactions' }} component={Transactions} />
-        </Navigator>
-      </NavigationContainer>
-    )
+import {Home, Transactions, ViewTransaction} from '../screens';
+
+const RootNavigation = ({}) => {
+  const {Screen, Navigator} = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Navigator initialRouteName="Home" headerMode="none">
+        <Screen name="Home" options={{title: 'Home'}} component={Home} />
+        <Screen
+          name="Transactions"
+          options={{title: 'Transactions'}}
+          component={Transactions}
+        />
+        <Screen
+          name="ViewTransaction"
+          options={{title: 'View Transaction'}}
+          component={ViewTransaction}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
 };
 export default RootNavigation;
