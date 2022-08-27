989d60e7e2980d3719e5ce389c0faa640cf38bcf
nikitadavies <nikita@keyvalue.systems>
2022-08-27 18:44:56 +0530

    Add transaction style fix

--- src/screens/Transactions/Transactions.tsx
+++ src/screens/Transactions/Transactions.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import styles from './styles';
 
 type TransactionListProps = {
   paidTo: string;
@@ -31,14 +34,17 @@ const dummyTransactionList: TransactionListProps[] = [
 ];
 
 const Transactions = ({navigation}: {navigation: any}) => {
-  console.log('transactions');
 
   const onViewTransaction = () => {
     navigation.navigate('ViewTransaction');
   };
 
   return (
-    <>
+     <LinearGradient
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      colors={['rgba(255, 255, 255, 0.8)', '#8082ED']}>
+       <View style={{ height: '100%' }} >
       {dummyTransactionList.map(transaction => (
         <Pressable style={styles.card} onPress={onViewTransaction}>
           <View style={styles.detail}>
@@ -57,45 +63,9 @@ const Transactions = ({navigation}: {navigation: any}) => {
           </View>
         </Pressable>
       ))}
-    </>
+      </View>
+      </LinearGradient>
   );
 };
 
 export default Transactions;
-
-const styles = StyleSheet.create({
-  card: {
-    padding: 16,
-    margin: 16,
-    backgroundColor: 'white',
-    borderRadius: 12,
-    marginBottom: 0,
-  },
-  detail: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  paidToText: {
-    fontSize: 15,
-    lineHeight: 16,
-    fontWeight: '700',
-    color: 'black',
-  },
-  amount: {
-    fontSize: 15,
-    lineHeight: 24,
-    fontWeight: '700',
-    color: '#004BE7',
-  },
-  date: {
-    fontSize: 11,
-    lineHeight: 16,
-    fontWeight: '400',
-    color: '#535353',
-  },
-  type: {
-    fontSize: 11,
-    lineHeight: 16,
-    fontWeight: '400',
-  },
-});
