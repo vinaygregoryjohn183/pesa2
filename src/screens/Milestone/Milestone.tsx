0ab63d534e57a2ca17d860a2bd4133e40913b834
Vinay Gregory John <vinay@keyvalue.systems>
2022-08-28 02:09:48 +0530

    Add milestone page

--- src/screens/Milestone/Milestone.tsx
+++ src/screens/Milestone/Milestone.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { RadioButton } from 'react-native-radio-buttons-group';
+
+import styles from './styles';
+
+enum MilestonePeriods {
+    Weekly = 'Weekly',
+    Monthly = 'Monthly'
+}
 
 const Milestone = () => {
+    const [budget, setBudget] = useState('');
+    const [period, setPeriod] = useState<MilestonePeriods>(MilestonePeriods.Weekly);
+
     return (
-        <View>
-            <Text>
-              Milestone
-            </Text>
+    <View>
+      <LinearGradient
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        colors={['rgba(255, 255, 255, 0)', '#8082EA']}
+        style={styles.container}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.label}>Period</Text>
+          <RadioButton id={MilestonePeriods.Weekly} color="#6264FF" label="Weekly" size={15} containerStyle={{ marginLeft: 0, marginTop: 12 }} labelStyle={styles.radioLabel} onPress={() => setPeriod(MilestonePeriods.Weekly)} selected={period === MilestonePeriods.Weekly} />
+          <RadioButton id={MilestonePeriods.Monthly} color="#6264FF" label="Monthly" size={15} containerStyle={{ marginLeft: 0, marginBottom: 30 }} labelStyle={styles.radioLabel} onPress={() => setPeriod(MilestonePeriods.Monthly)} selected={period === MilestonePeriods.Monthly} />
+
+          <Text style={styles.label}>Maximum Spend limit</Text>
+          <TextInput placeholder="Enter your budget" selectionColor="#3F60A6" value={budget}  onChange={(e) => setBudget(e.nativeEvent.text)} style={{ borderBottomColor: '#2B4985', borderBottomWidth: 1, width: '40%', padding: 0 }} />
         </View>
+      </LinearGradient>
+    </View>
     );
 };
 
